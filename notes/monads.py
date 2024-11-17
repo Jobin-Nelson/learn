db8e2c8efb684866332ad30eeb6b3a1c29516d6a
jobin_nelson <jobinnelson369@gmail.com>
2024-11-17 22:08:47 +0530

    refactor: convert Either to EitherABC

--- notes/monads.py
+++ notes/monads.py
@@ -1,6 +1,6 @@
 from abc import ABC, abstractmethod
 from functools import partial
-from typing import TypeVar, Callable, Generic
+from typing import TypeAlias, TypeVar, Callable, Generic
 
 T = TypeVar('T')  # Success type
 E = TypeVar('E')  # Error type
@@ -9,23 +9,23 @@ U = TypeVar('U')  # Return success type
 
 class Maybe(Generic[T]):
     def __init__(self, value: T) -> None:
-        self._value = value
+        self.value = value
 
     def map(self, f: Callable[[T], U]) -> 'Maybe[T] | Maybe[U]':
         if self.is_empty():
             return self
-        return Maybe(f(self._value))
+        return Maybe(f(self.value))
 
     def flat_map(self, f: Callable[[T], 'Maybe[U]']) -> 'Maybe[T] | Maybe[U]':
         if self.is_empty():
             return self
-        return f(self._value)
+        return f(self.value)
 
     def is_empty(self) -> bool:
-        return self._value is None
+        return self.value is None
 
 
-class Either(ABC, Generic[T, E]):
+class EitherABC(ABC, Generic[T, E]):
     def is_left(self) -> bool:
         return not self.is_right()
 
@@ -39,27 +39,27 @@ class Either(ABC, Generic[T, E]):
         raise NotImplementedError()
 
     @abstractmethod
-    def map(self, func: Callable[[T], U]) -> 'Either[T, E] | Either[U, E]':
+    def map(self, func: Callable[[T], U]) -> 'EitherABC[T, E] | EitherABC[U, E]':
         raise NotImplementedError()
 
     @abstractmethod
     def flat_map(
-        self, func: Callable[[T], 'Either[U, E]']
-    ) -> 'Either[T, E] | Either[U, E]':
+        self, func: Callable[[T], 'EitherABC[U, E]']
+    ) -> 'EitherABC[T, E] | EitherABC[U, E]':
         raise NotImplementedError()
 
 
-class Left(Either[T, E]):
+class Left(EitherABC[T, E]):
     def __init__(self, value: E) -> None:
         self._error = value
 
     def is_right(self) -> bool:
         return False
 
-    def map(self, func: Callable[[T], U]) -> Either[T, E]:
+    def map(self, func: Callable[[T], U]) -> EitherABC[T, E]:
         return self  # No transformation on Left
 
-    def flat_map(self, func: Callable[[T], Either[U, E]]) -> Either[T, E]:
+    def flat_map(self, func: Callable[[T], EitherABC[U, E]]) -> EitherABC[T, E]:
         return self  # No transformation on Left
 
     @property
@@ -67,41 +67,43 @@ class Left(Either[T, E]):
         return self._error
 
 
-class Right(Either[T, E]):
+class Right(EitherABC[T, E]):
     def __init__(self, value: T) -> None:
         self._value = value
 
     def is_right(self) -> bool:
         return True
 
-    def map(self, func: Callable[[T], U]) -> Either[U, E]:
+    def map(self, func: Callable[[T], U]) -> EitherABC[U, E]:
         return Right(func(self._value))
 
-    def flat_map(self, func: Callable[[T], Either[U, E]]) -> Either[U, E]:
+    def flat_map(self, func: Callable[[T], EitherABC[U, E]]) -> EitherABC[U, E]:
         return func(self._value)
 
     @property
     def value(self) -> T:
         return self._value
 
+Either: TypeAlias = Left | Right
+
 
 # Example usage
-def safe_divide(a: float, b: float) -> Either[float, str]:
+def safe_divide(a: float, b: float) -> EitherABC[float, str]:
     if b == 0:
         return Left("Division by zero")
     return Right(a / b)
 
 
-def return_left(_: float) -> Either[float, str]:
+def return_left(_: float) -> EitherABC[float, str]:
     return Left('Division by zero')
 
 
-result1: Either[float, str] = safe_divide(10, 2).flat_map(partial(safe_divide, b=0))
-result2: Either[float, str] = safe_divide(10, 2).flat_map(partial(safe_divide, b=2))
-result3: Either[float, str] = safe_divide(10, 2).map(lambda x: x + 10)
+result1: EitherABC[float, str] = safe_divide(10, 2).flat_map(partial(safe_divide, b=0))
+result2: EitherABC[float, str] = safe_divide(10, 2).flat_map(partial(safe_divide, b=2))
+result3: EitherABC[float, str] = safe_divide(10, 2).map(lambda x: x + 10)
 
 
-def check_result(res: Either[float, str]) -> None:
+def check_result(res: EitherABC[float, str]) -> None:
     if res.is_right():
         print("Result:", res.value)  # Output: Result: 5.0
     else:
